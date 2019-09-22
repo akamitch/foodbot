7bb5bb46df3569d95e3f093761d0d5db604171c6
Mitch <mitch@aka-root.com>
2019-09-23 00:24:42 +0300

    передвинул configs

--- db.php
+++ db.php
@@ -1,8 +1,9 @@
 <?php
-$configs = include('config.php');
+
 //config, pwd for mysql test after merge 22222
 
 function dbConnect($sql){
+    $configs = include('config.php');
 	$mysqli = new mysqli($config['hostname'], $config['dbuser'], $config['dbpassword'], $config['dbname']);
 	$utf="set names utf8"; //magic for cyrylic charsets
 	$mysqli->query($utf);
